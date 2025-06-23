6870cc0b249d8d22b4877153f14a221d992e4b40
kwangminleee <nuewaz@naver.com>
2025-06-23 14:53:42 +0900

    ✨ feat: Add PlayerInputActions & Movement system
    - 플레이어 인풋 액션(Input System) 설정
    - 이동 처리용 스크립트(PlayerMover) 작성

---
+++ Assets/1. Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private PlayerInputActions inputActions;
+
+    public Vector2 MoveInput { get; private set; }
+    public bool IsInteracting { get; private set; }
+
+    public System.Action OnInventoryToggle;
+    public System.Action OnBuildMode;
+    public System.Action OnDestroyMode;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Enable();
+
+        // 이동
+        inputActions.Player.Move.performed += ctx => MoveInput = ctx.ReadValue<Vector2>();
+        inputActions.Player.Move.canceled += _ => MoveInput = Vector2.zero;
+
+        // 상호작용 (공격 등)
+        inputActions.Player.Interaction.performed += _ => IsInteracting = true;
+        inputActions.Player.Interaction.canceled += _ => IsInteracting = false;
+
+        // 인벤토리
+        inputActions.Player.Inventory.performed += _ => OnInventoryToggle?.Invoke();
+
+        // 건설
+        inputActions.Player.Build.performed += _ => OnBuildMode?.Invoke();
+
+        // 해체
+        inputActions.Player.Destroy.performed += _ => OnDestroyMode?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+}
---
+++ Assets/1. Scripts/Player/PlayerMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMover : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;
+    private Rigidbody2D rb;
+    private PlayerInputHandler input;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        input = GetComponent<PlayerInputHandler>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (input == null) return;
+
+        Vector2 move = input.MoveInput;
+        rb.MovePosition(rb.position + move.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
