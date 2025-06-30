61c2790fff3f192c8a7a5020e91cda8dc4225eda
Hyobin <37098177+ahb0923@users.noreply.github.com>
2025-06-30 15:06:41 +0900

    ✨ feat: Helper_Transform Update
    -거리 계산 메서드 추가

--- Assets/1. Scripts/Utils/Helper/Helper_Transform.cs
+++ Assets/1. Scripts/Utils/Helper/Helper_Transform.cs
@@ -30,6 +30,8 @@ public static class Helper_Transform
     /// <summary>
     /// Transform의 X만 변경
     /// </summary>
+    /// <param name="t">자신</param>
+    /// <param name="x">x좌표</param>
     public static void SetX(this Transform t, float x)
     {
         Vector3 pos = t.position;
@@ -40,6 +42,8 @@ public static class Helper_Transform
     /// <summary>
     /// Transform의 Y만 변경
     /// </summary>
+    /// <param name="t">자신</param>
+    /// <param name="y">y좌표</param>
     public static void SetY(this Transform t, float y)
     {
         Vector3 pos = t.position;
@@ -47,5 +51,16 @@ public static class Helper_Transform
         t.position = pos;
     }
 
+    /// <summary>
+    /// 두개의 트랜스폼 사이의 근접도 체크
+    /// </summary>
+    /// <param name="a">첫번째 Transform</param>
+    /// <param name="b">두번째 Transform</param>
+    /// <param name="tolerance">오차 허용치, 기본값 0.01f</param>
+    /// <returns>두 Transform의 거리 차이가 오차 허용치 보다 작으면 true, 크면 false</returns>
+    public static bool IsCloseTo(this Transform a, Transform b, float tolerance = 0.01f)
+    {
+        return Vector3.Distance(a.position, b.position) <= tolerance;
+    }
 }
 
