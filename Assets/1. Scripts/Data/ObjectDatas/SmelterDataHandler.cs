951ca942bfb674478eecee528534acac30fed8c7
Hyobin <37098177+ahb0923@users.noreply.github.com>
2025-07-07 11:54:32 +0900

    ✨ feat: add SmleterData_Json
    제련소 데이터 및 데이터 핸들러 추가

---
+++ Assets/1. Scripts/Data/ObjectDatas/SmelterDataHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmelterDataHandler : BaseDataHandler<SmelterData>
+{
+    protected override string FileName => "SmelterData_JSON.json";
+
+    protected override int GetId(SmelterData data) => data.id;
+
+    protected override string GetName(SmelterData data) => data.smelterName;
+}
